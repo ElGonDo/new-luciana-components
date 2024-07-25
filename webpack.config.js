a8797414f85da6a9c52a241856b15148db425bcc
Joseph López Henao <joseph.lopez2@soy.sena.edu.co>
2024-07-25 00:13:25 -0500

    cambio en webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Importa el plugin
 
-   module.exports = {
-       entry: './src/index.ts', // Punto de entrada de tu aplicación
-       output: {
-           filename: 'bundle.js', // Nombre del archivo de salida
-           path: path.resolve(__dirname, 'dist'), // Carpeta de salida
-           library: 'new-luciana-components', // Nombre de la biblioteca
-           libraryTarget: 'umd', // Formato de la biblioteca (Universal Module Definition)
-       },
-       resolve: {
-           extensions: ['.tsx', '.ts', '.js'], // Extensiones que Webpack debe resolver
-       },
-       module: {
-           rules: [
-               {
-                   test: /\.tsx?$/, // Archivos TypeScript
-                   use: 'ts-loader', // Cargador para TypeScript
-                   exclude: /node_modules/,
-               },
-               {
-                   test: /\.module\.css$/, // Archivos CSS
-                   use: ['style-loader', 'css-loader'], // Cargadores para CSS
-               },
-           ],
-       },
-       mode: 'development', // Modo de desarrollo
-   };
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'new-luciana-components',
+        libraryTarget: 'umd',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.module\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    mode: 'development',
+};
